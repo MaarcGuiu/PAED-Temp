64bacd2445edf2d5168d4db94e71a06ec4e95aea
marc.guiu <marc.guiu@students.salle.url.edu>
2025-04-15 16:58:04 +0200

    repre

--- src/Fase3/UI/RepresentTTree.java
+++ src/Fase3/UI/RepresentTTree.java
@@ -1,33 +1,57 @@
 package Fase3.UI;
 
 import Fase3.RTree.Jugador;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYItemRenderer;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 
+import javax.swing.*;
+import java.awt.*;
 import java.util.List;
 
-public class RepresentTTree {
-    private static final int WIDTH = 50;
-    private static final int HEIGHT = 20;
+public class RepresentTTree extends JFrame {
 
     public static void representGraficament(List<Jugador> jugadors) {
-        char[][] grid = new char[HEIGHT][WIDTH];
+        XYSeries series = new XYSeries("Jugadors");
 
-        for (int i = 0; i < HEIGHT; i++) {
-            for (int j = 0; j < WIDTH; j++) {
-                grid[i][j] = '.';
-            }
+        for (Jugador jugador : jugadors) {
+            series.add(jugador.getBattlesDone(), jugador.getBattlesWon());
         }
 
-        int maxX = jugadors.stream().mapToInt(Jugador::getBattlesDone).max().orElse(1);
-        int maxY = jugadors.stream().mapToInt(Jugador::getBattlesWon).max().orElse(1);
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        dataset.addSeries(series);
 
-        for (Jugador j : jugadors) {
-            int x = (int) ((j.getBattlesDone() / (double) maxX) * (WIDTH - 1));
-            int y = HEIGHT - 1 - (int) ((j.getBattlesWon() / (double) maxY) * (HEIGHT - 1));
-            grid[y][x] = 'J';
-        }
+        JFreeChart scatterPlot = ChartFactory.createScatterPlot(
+                "Representació Gràfica R-Tree",
+                "Batalles fetes",
+                "Batalles guanyades",
+                dataset,
+                PlotOrientation.VERTICAL,
+                true, true, false
+        );
 
-        for (char[] row : grid) {
-            System.out.println(new String(row));
-        }
+        XYPlot plot = scatterPlot.getXYPlot();
+        XYItemRenderer renderer = new XYLineAndShapeRenderer(false, true);
+        plot.setRenderer(renderer);
+
+        plot.setBackgroundPaint(Color.white);
+        plot.setDomainGridlinePaint(Color.lightGray);
+        plot.setRangeGridlinePaint(Color.lightGray);
+
+        ChartPanel panel = new ChartPanel(scatterPlot);
+        panel.setPreferredSize(new Dimension(800, 600));
+
+        RepresentTTree frame = new RepresentTTree();
+        frame.setContentPane(panel);
+        frame.setTitle("Representació gràfica R-Tree");
+        frame.setSize(800, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
